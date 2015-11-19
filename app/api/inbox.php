20d511c90a13493a557abd9ab96d6b5c6abc9ce3
Hwei <hihwei@gmail.com>
2015-11-19 15:14:30 +0800

    update for 3.1.5

---
+++ app/api/inbox.php
@@ -0,0 +1,201 @@
+<?php
+/*
++--------------------------------------------------------------------------
+|   WeCenter [#RELEASE_VERSION#]
+|   ========================================
+|   by WeCenter Software
+|   © 2011 - 2014 WeCenter. All Rights Reserved
+|   http://www.wecenter.com
+|   ========================================
+|   Support: WeCenter@qq.com
+|
++---------------------------------------------------------------------------
+*/
+
+if (!defined('IN_ANWSION'))
+{
+	die;
+}
+
+class inbox extends AWS_CONTROLLER
+{
+	public function setup()
+	{
+		HTTP::no_cache_header();
+	}
+
+	public function index_action()
+	{
+		$this->model('account')->update_inbox_unread($this->user_id);
+
+		if( empty( $_GET['per_page'] ) ) $_GET['per_page'] = get_setting('contents_per_page');
+
+		if ($inbox_dialog = $this->model('message')->get_inbox_message($_GET['page'], $_GET['per_page'], $this->user_id))
+		{
+			$inbox_total_rows = $this->model('message')->found_rows();
+
+			foreach ($inbox_dialog as $key => $val)
+			{
+				$dialog_ids[] = $val['id'];
+
+				if ($this->user_id == $val['recipient_uid'])
+				{
+					$inbox_dialog_uids[] = $val['sender_uid'];
+				}
+				else
+				{
+					$inbox_dialog_uids[] = $val['recipient_uid'];
+				}
+			}
+		}
+
+		if ($inbox_dialog_uids)
+		{
+			if ($users_info_query = $this->model('account')->get_user_info_by_uids($inbox_dialog_uids))
+			{
+				foreach ($users_info_query as $user)
+				{
+					$users_info[$user['uid']] = $user;
+				}
+			}
+		}
+
+		if ($dialog_ids)
+		{
+			$last_message = $this->model('message')->get_last_messages($dialog_ids);
+		}
+
+		if ($inbox_dialog)
+		{
+			foreach ($inbox_dialog as $key => $value)
+			{
+				if ($value['recipient_uid'] == $this->user_id AND $value['recipient_count']) // 当前处于接收用户
+				{
+					$data[$key]['user_name'] = $users_info[$value['sender_uid']]['user_name'];
+					$data[$key]['url_token'] = $users_info[$value['sender_uid']]['url_token'];
+
+					$data[$key]['unread'] = $value['recipient_unread'];
+					$data[$key]['count'] = $value['recipient_count'];
+
+					$data[$key]['uid'] = $value['sender_uid'];
+				}
+				else if ($value['sender_uid'] == $this->user_id AND $value['sender_count']) // 当前处于发送用户
+				{
+					$data[$key]['user_name'] = $users_info[$value['recipient_uid']]['user_name'];
+					$data[$key]['url_token'] = $users_info[$value['recipient_uid']]['url_token'];
+
+					$data[$key]['unread'] = $value['sender_unread'];
+					$data[$key]['count'] = $value['sender_count'];
+					$data[$key]['uid'] = $value['recipient_uid'];
+				}
+
+				$data[$key]['last_message'] = $last_message[$value['id']];
+				$data[$key]['update_time'] = $value['update_time'];
+				$data[$key]['id'] = $value['id'];
+			}
+		}
+
+		H::ajax_json_output(AWS_APP::RSM(array(
+			'total_rows' => $inbox_total_rows,
+			'rows' => $data
+		), 1, null));
+	
+	}
+
+
+	public function read_action()
+	{
+		if (!$dialog = $this->model('message')->get_dialog_by_id($_GET['id']))
+		{
+			H::redirect_msg(AWS_APP::lang()->_t('指定的站内信不存在'), '/inbox/');
+		}
+
+		if ($dialog['recipient_uid'] != $this->user_id AND $dialog['sender_uid'] != $this->user_id)
+		{
+			H::redirect_msg(AWS_APP::lang()->_t('指定的站内信不存在'), '/inbox/');
+		}
+
+		$this->model('message')->set_message_read($_GET['id'], $this->user_id);
+
+		if ($list = $this->model('message')->get_message_by_dialog_id($_GET['id']))
+		{
+			if ($dialog['sender_uid'] != $this->user_id)
+			{
+				$recipient_user = $this->model('account')->get_user_info_by_uid($dialog['sender_uid']);
+			}
+			else
+			{
+				$recipient_user = $this->model('account')->get_user_info_by_uid($dialog['recipient_uid']);
+			}
+
+			foreach ($list as $key => $val)
+			{
+				if ($dialog['sender_uid'] == $this->user_id AND $val['sender_remove'])
+				{
+					unset($list[$key]);
+				}
+				else if ($dialog['sender_uid'] != $this->user_id AND $val['recipient_remove'])
+				{
+					unset($list[$key]);
+				}
+				else
+				{
+					$list[$key]['message'] = FORMAT::parse_links($val['message']);
+
+					$list[$key]['user_name'] = $recipient_user['user_name'];
+					$list[$key]['url_token'] = $recipient_user['url_token'];
+				}
+			}
+		}
+
+		$user_key = array( 'uid', 'user_name', 'name', 'avatar_file', 'namecard_pic', 'signature' );
+
+		if( !empty( $recipient_user ) )
+		{
+			foreach ($recipient_user as $k => $v) 
+			{
+				 if( !in_array($k, $user_key) ) unset( $recipient_user[$k] );
+			}
+		}
+
+		$recipient_user['avatar_file'] = get_avatar_url($recipient_user['uid'],'max');
+
+		H::ajax_json_output(AWS_APP::RSM(array(
+            'recipient_user' => $recipient_user,
+            'rows' => $list
+        ), 1, null));
+
+	}
+
+
+
+	public function send_action()
+	{
+		if (trim($_POST['message']) == '')
+		{
+			H::ajax_json_output(AWS_APP::RSM(null, '-1', AWS_APP::lang()->_t('请输入私信内容')));
+		}
+
+		if (!$recipient_user = $this->model('account')->get_user_info_by_username($_POST['recipient']))
+		{
+			H::ajax_json_output(AWS_APP::RSM(null, '-1', AWS_APP::lang()->_t('接收私信的用户不存在')));
+		}
+
+		if ($recipient_user['uid'] == $this->user_id)
+		{
+			H::ajax_json_output(AWS_APP::RSM(null, '-1', AWS_APP::lang()->_t('不能给自己发私信')));
+		}
+
+		if ($recipient_user['inbox_recv'])
+		{
+			if (! $this->model('message')->check_permission($recipient_user['uid'], $this->user_id))
+			{
+				H::ajax_json_output(AWS_APP::RSM(null, '-1', AWS_APP::lang()->_t('对方设置了只有 Ta 关注的人才能给 Ta 发送私信')));
+			}
+		}
+
+		$this->model('message')->send_message($this->user_id, $recipient_user['uid'], $_POST['message']);
+
+		H::ajax_json_output(AWS_APP::RSM(null, 1, null));
+	}
+}
---
+++ app/api/people.php
@@ -0,0 +1,358 @@
+<?php
+/*
++--------------------------------------------------------------------------
+|   WeCenter [#RELEASE_VERSION#]
+|   ========================================
+|   by WeCenter Software
+|   © 2011 - 2014 WeCenter. All Rights Reserved
+|   http://www.wecenter.com
+|   ========================================
+|   Support: WeCenter@qq.com
+|
++---------------------------------------------------------------------------
+*/
+
+
+if (!defined('IN_ANWSION'))
+{
+	die;
+}
+
+class people extends AWS_CONTROLLER
+{
+
+	public function get_access_rule()
+	{
+		$rule_action['rule_type'] = 'white'; //黑名单,黑名单中的检查  'white'白名单,白名单以外的检查
+
+		$rule_action['actions'] = array(
+			'index',
+			'user_info',
+			'user_actions_by_where'
+		);
+
+		return $rule_action;
+	}
+
+	public function setup()
+	{
+		$this->per_page = get_setting('contents_per_page');
+
+		HTTP::no_cache_header();
+	}
+
+
+	//个人主页 获取用户信息
+	public function index_action()
+	{
+        $user = $this->model('account')->get_user_info_by_uid($_GET['uid'], TRUE);
+
+        if(!$user)
+        {
+        	H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('用户不存在')));
+        }
+
+        $this->model('people')->update_views($user['uid']);
+
+        //$user['job_id'] = get_dict('工作职位',$user['job_id']);
+
+        //教育经历
+		$education_experience_list = $this->model('education')->get_education_experience_list($user['uid']);
+
+
+        //工作经历
+        
+		$jobs_list = $this->model('work')->get_jobs_list();
+
+		if ($work_experience_list = $this->model('work')->get_work_experience_list($user['uid']))
+		{
+			foreach ($work_experience_list as $key => $val)
+			{
+				$work_experience_list[$key]['job_name'] = $jobs_list[$val['job_id']];
+			}
+		}
+		
+
+
+		if( $this->model('follow')->user_follow_check($this->user_id, $user['uid']) )
+		{
+			$user['has_follow'] = 1;
+		}
+
+		//擅长话题
+		//$reputation_topics = $this->model('people')->get_user_reputation_topic($user['uid'], $user['reputation'], 6);
+
+		/*
+		$fans_list = $this->model('follow')->get_user_fans($user['uid'], 5);
+		$friends_list = $this->model('follow')->get_user_friends($user['uid'], 5);
+		$focus_topics = $this->model('topic')->get_focus_topic_list($user['uid'], 5);
+		*/
+		
+		//clean
+		if( !empty( $user ) )
+		{
+			$user_key = array( 'uid', 'user_name', 'name', 'company', 'work', 'avatar_file', 'sex', 'job_id', 'fans_count', 'friend_count', 'invite_count', 'question_count', 'news_count', 'article_count', 'answer_count', 'topic_focus_count', 'agree_count', 'thanks_count', 'reputation', 'draft_count', 'yue_count', 'province', 'signature', 'city' );
+ 			if($this->user_id == $user['uid'])
+			{
+				$user_key[] = 'mobile'; 
+			}
+
+			foreach ($user as $k => $v) 
+			{
+				if( !in_array($k, $user_key) ) unset( $user[$k] );
+			}
+ 
+		}
+
+		//当前用户是否已关注该用户
+		$user['has_focus'] = 0;
+
+		if( $this->user_id AND ( $this->user_id != $user['uid'] ) )
+		{
+			//判断是否存在关注
+			if ($this->model('follow')->user_follow_check($this->user_id, $user['uid']))
+			{
+				$user['has_focus'] = 1;
+			}
+
+			unset($user['mobile']);
+		}
+		
+		if( !$this->user_id )
+		{
+			unset($user['mobile']);
+		}
+		
+
+		$user['avatar_file'] = get_avatar_url( $user['uid'], 'max' );
+
+		H::ajax_json_output(AWS_APP::RSM(array(
+                'user_info' => $user,
+                'education_experience_list' => $education_experience_list,
+                'work_experience_list' => $work_experience_list
+            ), 1, null));
+	}
+
+
+
+	public function user_actions_action()
+	{
+		if ((isset($_GET['perpage']) AND intval($_GET['perpage']) > 0))
+		{
+			$this->per_page = intval($_GET['perpage']);
+		}
+
+		$data = $this->model('actions')->get_user_actions($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}", $_GET['actions'], $this->user_id);
+
+		TPL::assign('list', $data);
+
+		if (is_mobile())
+		{
+			$template_dir = 'm';
+		}
+		else
+		{
+			$template_dir = 'people';
+		}
+
+		if ($_GET['actions'] == '201')
+		{
+			TPL::output($template_dir . '/ajax/user_actions_questions_201');
+		}
+		else if ($_GET['actions'] == '101')
+		{
+			TPL::output($template_dir . '/ajax/user_actions_questions_101');
+		}
+		else
+		{
+			TPL::output($template_dir . '/ajax/user_actions');
+		}
+	}
+
+
+    public function user_actions_by_where_action()
+    {
+        if ((isset($_GET['perpage']) AND intval($_GET['perpage']) > 0))
+        {
+            $this->per_page = intval($_GET['perpage']);
+        }
+        $associate_type = $_GET['type'];
+
+        if(!in_array($associate_type,array(ACTION_LOG::CATEGORY_ARTICLE,
+                                            ACTION_LOG::CATEGORY_QUESTION,
+                                            ACTION_LOG::CATEGORY_ANSWER,
+                                            ACTION_LOG::CATEGORY_NEWS)))
+        {
+            H::ajax_json_output(AWS_APP::RSM(null, -1, AWS_APP::lang()->_t('参数有误')));
+        }
+
+        $associate_action = $_GET['actions'];
+
+        if( empty( $_GET['page'] ) )  $_GET['page'] = 0;
+				 
+				if($data = $this->model('actions')->get_user_action_by_where($associate_type , $associate_action ,$_GET['uid'] ,(intval($_GET['page']) * $this->per_page) . ", {$this->per_page}"))
+				{
+		    	foreach ($data as $k => $v)
+					{
+						if( !empty( $v['info']['message'] ) AND ( strlen( $v['info']['message'] ) > 80 ) )
+						{
+							 $data[$k]['info']['message'] =  cjk_substr(strip_tags( $v['info']['message'] ),0,80,'utf-8').'...';
+						}
+					}
+				}else
+				{
+					$data = null;
+				}
+					
+        H::ajax_json_output(AWS_APP::RSM(array(
+					'total_rows' => count( $data ),
+					'rows' => $data
+			), 1, null));
+
+    }
+
+
+    public function user_info_action()
+	{
+		if ($this->user_id == $_GET['uid'])
+		{
+			$user_info = $this->user_info;
+		}
+		else if (!$user_info = $this->model('account')->get_user_info_by_uid($_GET['uid'], ture))
+		{
+			H::ajax_json_output(array(
+				'uid' => null
+			));
+		}
+
+		if ($this->user_id != $user_info['uid'])
+		{
+			$user_follow_check = $this->model('follow')->user_follow_check($this->user_id, $user_info['uid']);
+		}
+
+		H::ajax_json_output(array(
+			'reputation' => $user_info['reputation'],
+			'agree_count' => $user_info['agree_count'],
+			'thanks_count' => $user_info['thanks_count'],
+			'type' => 'people',
+			'uid' => $user_info['uid'],
+			'user_name' => $user_info['user_name'],
+			'avatar_file' => get_avatar_url($user_info['uid'], 'mid'),
+			'signature' => $user_info['signature'],
+			'focus' => ($user_follow_check ? true : false),
+			'is_me' => (($this->user_id == $user_info['uid']) ? true : false),
+			'url' => get_js_url('/people/' . $user_info['url_token']),
+			'category_enable' => ((get_setting('category_enable') == 'Y') ? 1 : 0),
+			'verified' => $user_info['verified'],
+			'fans_count' => $user_info['fans_count']
+		));
+	}
+
+	public function follows_action()
+	{
+		switch ($_GET['type'])
+		{
+			case 'follows':
+				$users_list = $this->model('follow')->get_user_friends($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}");
+			break;
+
+			case 'fans':
+				$users_list = $this->model('follow')->get_user_fans($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}");
+			break;
+		}
+
+		if ($users_list AND $this->user_id)
+		{
+			foreach ($users_list as $key => $val)
+			{
+				$users_ids[] = $val['uid'];
+			}
+
+			if ($users_ids)
+			{
+				$follow_checks = $this->model('follow')->users_follow_check($this->user_id, $users_ids);
+
+				foreach ($users_list as $key => $val)
+				{
+					$users_list[$key]['follow_check'] = $follow_checks[$val['uid']];
+				}
+			}
+		}
+
+		
+
+		$user_key = array( 'uid', 'user_name', 'name', 'avatar_file', 'namecard_pic', 'signature', 'reputation', 'agree_count' );
+
+		if( !empty( $users_list ) )
+		{
+			foreach ($users_list as $key => $value) 
+			{
+				foreach ($value as $k => $v) {
+					if( !in_array($k, $user_key) ) unset( $value[$k] );
+				}
+                $value['avatar_file'] = get_avatar_url($value['uid'],'mid');
+                $ret[] = $value;
+			}
+		}
+
+	   	H::ajax_json_output(AWS_APP::RSM(array(
+				'total_rows' => count( $users_list ),
+				'rows' => $ret
+		), 1, null));
+	}
+
+	public function topics_action()
+	{
+		if ($topic_list = $this->model('topic')->get_focus_topic_list($_GET['uid'], (intval($_GET['page']) * $this->per_page) . ", {$this->per_page}") AND $this->user_id)
+		{
+			$topic_ids = array();
+
+			foreach ($topic_list as $key => $val)
+			{
+				$topic_ids[] = $val['topic_id'];
+			}
+
+			if ($topic_ids)
+			{
+				$topic_focus = $this->model('topic')->has_focus_topics($this->user_id, $topic_ids);
+
+				foreach ($topic_list as $key => $val)
+				{
+					$topic_list[$key]['has_focus'] = $topic_focus[$val['topic_id']];
+				}
+			}
+		}
+
+		H::ajax_json_output(AWS_APP::RSM(array(
+				'total_rows' => count( $topic_list ),
+				'rows' => $topic_list
+		), 1, null));
+	}
+
+
+    public function favorite_action()
+    {
+        if ($action_list = $this->model('favorite')->get_item_list($_GET['tag'], $this->user_id, calc_page_limit($_GET['page'], get_setting('contents_per_page'))))
+        {
+            foreach ($action_list AS $key => $val)
+            {
+                $item_ids[] = $val['item_id'];
+            }
+        }
+        else
+        {
+            if (!$_GET['page'] OR $_GET['page'] == 1)
+            {
+                $this->model('favorite')->remove_favorite_tag(null, null, $_GET['tag'], $this->user_id);
+            }
+        }
+
+        H::ajax_json_output(AWS_APP::RSM(array(
+            'total_rows' => count( $action_list ),
+            'rows' => $action_list
+        ), 1, null));
+
+
+
+    }
+}
