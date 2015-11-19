5d2307853a33da36eb366e9b9b6bdfbceb254262
Hwei <hihwei@gmail.com>
2015-11-19 15:09:57 +0800

    update

--- api/lib/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define( 'DB_HOST', '' );
-define( 'DB_USER', '' );
-define( 'DB_PWD', '' );
-define( 'DB_NAME', '' );
-define( 'TABLE_PREFIX', '' );
-
-define( 'ANSWER_PIC', 'http://www.example.com/uploads/answer/' );
-
